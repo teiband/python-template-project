1a8b9e945378cf08c2277e3350b46cd236336ced
teiband <teiband@users.noreply.github.com>
2021-06-17 14:16:44 +0200

    Create conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+# Contains all code that is commonly used in all pytests, e.g. some fixtures
+# It is automatically imported whenever a pytest is called.
+
+
+@pytest.fixture()
+def example_fixture():
+    fixture = 123
+    return fixture
